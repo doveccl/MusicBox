054ab5ec951422ce3fc5492031941a2e983f714a
Doveccl <doveccl@live.com>
2020-05-29 16:26:39 +0800

    feat: add song manager

--- src/cli.ts
+++ src/cli.ts
@@ -14,7 +14,9 @@ const commands = {
   l: 'list all users',
   a: 'add or update user',
   d: 'delete a user',
-  u: 'update all songs index',
+  u: 'update all songs index (ignore index start with !)',
+  s: 'search songs by title or artist (without index)',
+  m: 'modify song info',
   c: 'clear screen',
   h: 'show help info',
   e: 'exit cli tool'
@@ -48,8 +50,8 @@ createConnection().then(async () => {
       case 'add':
         try {
           const user = new User()
-          user.name = await question('> name: ')
-          user.password = await question('> password: ')
+          user.name = (await question('> name: ')) || user.name
+          user.password = (await question('> password: ')) || user.password
           user.admin = Number(await question('> admin: '))
           await user.save()
           console.table(user)
@@ -73,10 +75,41 @@ createConnection().then(async () => {
       case 'update':
         try {
           for (const song of await Song.find()) {
-            song.index = song.generateIndex()
-            await song.save()
-            console.log('updated:', song.index)
+            if (song.index.startsWith('!')) {
+              console.log('skip:', song.title, song.index)
+            } else {
+              song.index = song.generateIndex()
+              await song.save()
+              console.log('updated:', song.index)
+            }
           }
+        } catch (e) {
+          console.error(e)
+        }
+        break
+      case 's':
+      case 'search':
+        const search = '%' + await question('> search: ') + '%'
+        console.table(
+          await Song.createQueryBuilder()
+            .select(['id', 'title', 'artist', 'priority', 'deleted'])
+            .addSelect('SUBSTR("index", 0, 10) || "..."', 'index')
+            .where('title LIKE :search', { search })
+            .orWhere('artist LIKE :search', { search })
+            .getRawMany()
+        )
+        break
+      case 'm':
+      case 'modify':
+        const id = Number(await question('> song id: '))
+        try {
+          const song = await Song.findOneOrFail({ id })
+          console.log('FOUND', song)
+          song.title = (await question('> title: ')) || song.title
+          song.artist = (await question('> artist: ')) || song.artist
+          song.index = (await question('> index: ')) || song.generateIndex()
+          console.log('MODIFY', song)
+          await song.save()
         } catch (e) {
           console.error(e)
         }
